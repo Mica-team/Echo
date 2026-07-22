c03b521d92fc6d0e4692e521d6e45fcb95fba63a
jimj90201-a11y <jimj90201@gmail.com>
2026-07-22 20:06:21 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application") version "8.6.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
-
+    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
     id("com.google.gms.google-services") version "4.4.3" apply false
 }
